bfaadb575981f0ad19ff415f00f80257bacb968b
rorro <rodguzm03@gmail.com>
2024-05-31 01:06:14 -0400

    mas juegos

--- videojuego.js
+++ videojuego.js
@@ -2,28 +2,142 @@ const videojuego = [
     {
 
         "id":1,
-        "nombre":"euro truck simulator",
-        "desarrollador": "scs software"
+        "nombre":"star wars: jfo",
+        "precio": 12.000,
+        "desarrollador": "respawn"
 
     },
     {
         "id":2,
-        "nombre":"call of duty: black Ops 6",
-        "desarrollador":"treyarch"
+        "nombre":"red dead redemption",
+        "precio": 11.000,
+        "desarrollador":"rockstar games"
     },
     {
         "id":3,
-        "nombre":"eafc 24",
-        "desarrollador":"electronics arts"
+        "nombre":"sot: tomb raider",
+        "precio": 6.000,
+        "desarrollador":"square enix"
     },
     {
         "id":4,
-        "nombre":"counter strike 2 ",
-        "desarrollador":"valve"
-    }
+        "nombre":"grand theft auto v",
+        "precio": 10.000,
+        "desarrollador":"rockstar games"
+    },
+    {
 
+        "id":5,
+        "nombre":"need for speed: heat deluxe edition",
+        "precio": 8.000,
+        "desarrollador": "ghost"
 
+    },
+    {
 
+        "id":6,
+        "nombre":"eafc 24",
+        "precio": 48.000,
+        "desarrollador": "electronics arts"
+
+    },
+    {
+
+        "id":7,
+        "nombre":"multi pack - dk, mb ods, smb dlx",
+        "precio": 42.500,
+        "desarrollador": "nintendo"
+
+    },
+    {
+
+        "id":8,
+        "nombre":"minecraft",
+        "precio": 12.990,
+        "desarrollador": "mojang"
+
+    },
+    {
+
+        "id":9,
+        "nombre":"animal crossing dlc new horizons",
+        "precio": 23.500,
+        "desarrollador": "nintendo"
+
+    },
+    {
+
+        "id":10,
+        "nombre":"zelda botw",
+        "precio": 52.500,
+        "desarrollador": "nintendo"
+
+    },
+    {
+
+        "id":11,
+        "nombre":"gta the trylogy: definitive edition",
+        "precio": 15.600,
+        "desarrollador": "rockstar games"
+
+    },
+    {
+
+        "id":12,
+        "nombre":"db: xenoverse 2",
+        "precio": 23.500,
+        "desarrollador": "nintendo"
+
+    },
+    {
+
+        "id":13,
+        "nombre":"uncharted collection: remastered",
+        "precio": 32.000,
+        "desarrollador": "scs software"
+
+    },
+    {
+
+        "id":14,
+        "nombre":"cyberpunk 2077",
+        "precio": 32.000,
+        "desarrollador": "cd project red"
+
+    },
+    {
+
+        "id":15,
+        "nombre":"call of duty: modern warfare 3 2023",
+        "precio": 47.650,
+        "desarrollador": "sledghamer games"
+
+    },
+    {
+
+        "id":16,
+        "nombre":"gran turismo 7",
+        "precio": 52.500,
+        "desarrollador": "playstation studios"
+
+    },
+    {
+
+        "id":17,
+        "nombre":"god of war ragnarok",
+        "precio": 25.600,
+        "desarrollador": "santa monica studio"
+
+    },
+    {
+
+        "id":18,
+        "nombre":"ghost of tsushima: directors cut",
+        "precio": 38.500,
+        "desarrollador": "playstation studios"
+
+    },
+    
 ];
 
 module.exports = {
