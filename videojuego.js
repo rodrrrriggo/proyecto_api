526347dea132118ab2e5887e1287578f2cb6c6a5
rorro <rodguzm03@gmail.com>
2024-06-02 20:05:20 -0400

    agregar fotos

--- videojuego.js
+++ videojuego.js
@@ -2,6 +2,7 @@ const videojuego = [
     {
 
         "id":1,
+        "imagen": "https://image.api.playstation.com/vulcan/img/rnd/202105/1714/WHeOu95nW2SZQy6H5IKgE2Bg.png", 
         "nombre":"star wars: jfo",
         "precio": 12.554,
         "desarrollador": "respawn"
@@ -9,18 +10,21 @@ const videojuego = [
     },
     {
         "id":2,
+        "imagen": "https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png",
         "nombre":"red dead redemption",
         "precio": 11.112,
         "desarrollador":"rockstar games"
     },
     {
         "id":3,
+        "imagen": "https://pics.filmaffinity.com/Shadow_of_the_Tomb_Raider-862567947-large.jpg",
         "nombre":"sot: tomb raider",
         "precio": 6.666,
         "desarrollador":"square enix"
     },
     {
         "id":4,
+        "imagen": "https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_1200x1600_Portrait%20Store%20Banner-1200x1600-382243057711adf80322ed2aeea42191.jpg",
         "nombre":"grand theft auto v",
         "precio": 10.222,
         "desarrollador":"rockstar games"
@@ -28,6 +32,7 @@ const videojuego = [
     {
 
         "id":5,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202210/3121/9y8pTbxVRpmB9qqFOiKsaJgA.png",
         "nombre":"need for speed: heat deluxe edition",
         "precio": 8.555,
         "desarrollador": "ghost"
@@ -36,6 +41,7 @@ const videojuego = [
     {
 
         "id":6,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202310/0214/c21c0669eaca7f3e02c41cad5e1a20f867aafcf14104f046.png",
         "nombre":"eafc 24",
         "precio": 48.888,
         "desarrollador": "electronics arts"
@@ -44,6 +50,7 @@ const videojuego = [
     {
 
         "id":7,
+        "imagen": "https://juegosdigitaleschile.com/files/images/productos/1673544997-super-summer-pack-2-nintendo-switch-0.jpg",
         "nombre":"multi pack - dk, mb ods, smb dlx",
         "precio": 42.555,
         "desarrollador": "nintendo"
@@ -52,6 +59,7 @@ const videojuego = [
     {
 
         "id":8,
+        "imagen": "https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png",
         "nombre":"minecraft",
         "precio": 12.999,
         "desarrollador": "mojang"
@@ -60,6 +68,7 @@ const videojuego = [
     {
 
         "id":9,
+        "imagen": "https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg",
         "nombre":"animal crossing dlc new horizons",
         "precio": 23.555,
         "desarrollador": "nintendo"
@@ -68,6 +77,7 @@ const videojuego = [
     {
 
         "id":10,
+        "imagen": "https://zelda.nintendo.com/breath-of-the-wild/assets/media/wallpapers/tablet-1.jpg",
         "nombre":"zelda botw",
         "precio": 52.555,
         "desarrollador": "nintendo"
@@ -76,6 +86,7 @@ const videojuego = [
     {
 
         "id":11,
+        "imagen": "https://juegosdigitaleschile.com/files/images/productos/1637860688-grand-theft-auto-the-trilogy-the-definitive-edition-nintendo-switch.jpg",
         "nombre":"gta the trylogy: definitive edition",
         "precio": 15.666,
         "desarrollador": "rockstar games"
@@ -84,6 +95,7 @@ const videojuego = [
     {
 
         "id":12,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202312/2208/e4b4b4b9bba306e560f5ea1a6ea985692cce579f8b826c0b.png",
         "nombre":"db: xenoverse 2",
         "precio": 23.555,
         "desarrollador": "nintendo"
@@ -92,6 +104,7 @@ const videojuego = [
     {
 
         "id":13,
+        "imagen": "https://image.api.playstation.com/vulcan/img/rnd/202011/1101/ry4VLjdf1G2u47FcmJtgVsFq.png",
         "nombre":"uncharted collection: remastered",
         "precio": 32.787,
         "desarrollador": "scs software"
@@ -100,6 +113,7 @@ const videojuego = [
     {
 
         "id":14,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202111/3013/6bAF2VVEamgKclalI0oBnoAe.jpg",
         "nombre":"cyberpunk 2077",
         "precio": 32.333,
         "desarrollador": "cd project red"
@@ -108,6 +122,7 @@ const videojuego = [
     {
 
         "id":15,
+        "imagen": "https://i.blogs.es/beed80/call-of-duty-modern-warfare-3-resena/375_375.jpeg",
         "nombre":"call of duty: modern warfare 3 2023",
         "precio": 47.655,
         "desarrollador": "sledghamer games"
@@ -116,6 +131,7 @@ const videojuego = [
     {
 
         "id":16,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202109/1321/yZ7dpmjtHr1olhutHT57IFRh.png",
         "nombre":"gran turismo 7",
         "precio": 52.555,
         "desarrollador": "playstation studios"
@@ -124,6 +140,7 @@ const videojuego = [
     {
 
         "id":17,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png",
         "nombre":"god of war ragnarok",
         "precio": 25.666,
         "desarrollador": "santa monica studio"
@@ -132,6 +149,7 @@ const videojuego = [
     {
 
         "id":18,
+        "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202106/2322/v53I7qHvpWSQsL2oLxrM46NV.png",
         "nombre":"ghost of tsushima: directors cut",
         "precio": 38.555,
         "desarrollador": "playstation studios"
