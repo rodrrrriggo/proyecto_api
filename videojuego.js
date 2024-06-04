326f09215b3c8ab6eed26605add7e973b5ad1a8d
rorro <rodguzm03@gmail.com>
2024-06-03 22:29:46 -0400

    cambios a titulos de juegos

--- videojuego.js
+++ videojuego.js
@@ -3,156 +3,156 @@ const videojuego = [
 
         "id":1,
         "imagen": "https://image.api.playstation.com/vulcan/img/rnd/202105/1714/WHeOu95nW2SZQy6H5IKgE2Bg.png", 
-        "nombre":"star wars: jfo",
+        "nombre":"STAR WARS: Jedi Fallen Order",
         "precio": 12.554,
-        "desarrollador": "respawn"
+        "desarrollador": "Respawn Entertainment"
 
     },
     {
         "id":2,
         "imagen": "https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png",
-        "nombre":"red dead redemption",
+        "nombre":"Red Dead Redemption",
         "precio": 11.112,
-        "desarrollador":"rockstar games"
+        "desarrollador":"Rockstar Games"
     },
     {
         "id":3,
         "imagen": "https://pics.filmaffinity.com/Shadow_of_the_Tomb_Raider-862567947-large.jpg",
-        "nombre":"sot: tomb raider",
+        "nombre":"Shadow Of The Tomb Raider",
         "precio": 6.666,
-        "desarrollador":"square enix"
+        "desarrollador":"Square Enix"
     },
     {
         "id":4,
         "imagen": "https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_1200x1600_Portrait%20Store%20Banner-1200x1600-382243057711adf80322ed2aeea42191.jpg",
-        "nombre":"grand theft auto v",
+        "nombre":"Grand Theft Auto V",
         "precio": 10.222,
-        "desarrollador":"rockstar games"
+        "desarrollador":"Rockstar Games"
     },
     {
 
         "id":5,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202210/3121/9y8pTbxVRpmB9qqFOiKsaJgA.png",
-        "nombre":"need for speed: heat deluxe edition",
+        "nombre":"Need For Speed Heat: Deluxe Edition",
         "precio": 8.555,
-        "desarrollador": "ghost"
+        "desarrollador": "Ghost"
 
     },
     {
 
         "id":6,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202310/0214/c21c0669eaca7f3e02c41cad5e1a20f867aafcf14104f046.png",
-        "nombre":"eafc 24",
+        "nombre":"EAFC 24",
         "precio": 48.888,
-        "desarrollador": "electronics arts"
+        "desarrollador": "Electronics Arts: Sports"
 
     },
     {
 
         "id":7,
         "imagen": "https://juegosdigitaleschile.com/files/images/productos/1673544997-super-summer-pack-2-nintendo-switch-0.jpg",
-        "nombre":"multi pack - dk, mb ods, smb dlx",
+        "nombre":"Multi Pack - DK, MB ODS, SMB DLX",
         "precio": 42.555,
-        "desarrollador": "nintendo"
+        "desarrollador": "Nintendo"
 
     },
     {
 
         "id":8,
         "imagen": "https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png",
-        "nombre":"minecraft",
+        "nombre":"Minecraft",
         "precio": 12.999,
-        "desarrollador": "mojang"
+        "desarrollador": "Mojang"
 
     },
     {
 
         "id":9,
         "imagen": "https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg",
-        "nombre":"animal crossing dlc new horizons",
+        "nombre":"Animal Crossing DLC New Horizons",
         "precio": 23.555,
-        "desarrollador": "nintendo"
+        "desarrollador": "Nintendo"
 
     },
     {
 
         "id":10,
         "imagen": "https://zelda.nintendo.com/breath-of-the-wild/assets/media/wallpapers/tablet-1.jpg",
-        "nombre":"zelda botw",
+        "nombre":"Zelda Botw",
         "precio": 52.555,
-        "desarrollador": "nintendo"
+        "desarrollador": "Nintendo"
 
     },
     {
 
         "id":11,
         "imagen": "https://juegosdigitaleschile.com/files/images/productos/1637860688-grand-theft-auto-the-trilogy-the-definitive-edition-nintendo-switch.jpg",
-        "nombre":"gta the trylogy: definitive edition",
+        "nombre":"GTA The Trylogy: Definitive Edition",
         "precio": 15.666,
-        "desarrollador": "rockstar games"
+        "desarrollador": "Rockstar Games"
 
     },
     {
 
         "id":12,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202312/2208/e4b4b4b9bba306e560f5ea1a6ea985692cce579f8b826c0b.png",
-        "nombre":"db: xenoverse 2",
+        "nombre":"DB: Xenoverse 2",
         "precio": 23.555,
-        "desarrollador": "nintendo"
+        "desarrollador": "Nintendo"
 
     },
     {
 
         "id":13,
         "imagen": "https://image.api.playstation.com/vulcan/img/rnd/202011/1101/ry4VLjdf1G2u47FcmJtgVsFq.png",
-        "nombre":"uncharted collection: remastered",
+        "nombre":"Uncharted Collection: Remastered",
         "precio": 32.787,
-        "desarrollador": "scs software"
+        "desarrollador": "SCS Software"
 
     },
     {
 
         "id":14,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202111/3013/6bAF2VVEamgKclalI0oBnoAe.jpg",
-        "nombre":"cyberpunk 2077",
+        "nombre":"Cyberpunk 2077",
         "precio": 32.333,
-        "desarrollador": "cd project red"
+        "desarrollador": "CD Project Red"
 
     },
     {
 
         "id":15,
         "imagen": "https://i.blogs.es/beed80/call-of-duty-modern-warfare-3-resena/375_375.jpeg",
-        "nombre":"call of duty: modern warfare 3 2023",
+        "nombre":"Call of Duty: Modern Warfare 3 2023",
         "precio": 47.655,
-        "desarrollador": "sledghamer games"
+        "desarrollador": "Sledghamer Games"
 
     },
     {
 
         "id":16,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202109/1321/yZ7dpmjtHr1olhutHT57IFRh.png",
-        "nombre":"gran turismo 7",
+        "nombre":"Gran Turismo 7",
         "precio": 52.555,
-        "desarrollador": "playstation studios"
+        "desarrollador": "Playstation Studios"
 
     },
     {
 
         "id":17,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png",
-        "nombre":"god of war ragnarok",
+        "nombre":"God of War Ragnarok",
         "precio": 25.666,
-        "desarrollador": "santa monica studio"
+        "desarrollador": "Santa Monica Studio"
 
     },
     {
 
         "id":18,
         "imagen": "https://image.api.playstation.com/vulcan/ap/rnd/202106/2322/v53I7qHvpWSQsL2oLxrM46NV.png",
-        "nombre":"ghost of tsushima: directors cut",
+        "nombre":"Ghost of Tsushima: Directors Cut",
         "precio": 38.555,
-        "desarrollador": "playstation studios"
+        "desarrollador": "Playstation Studios"
 
     },
     
